fb62a24c75857def3eff11896471f7a6bf948768
Eduardo Trujillo <ed@sellerlabs.com>
2016-10-26 15:51:30 -0700

    chore(contrib): Improve auto-deploy script

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 changelog:
 	/bin/bash -c "docker run -v $(shell pwd)/.git:/.git quay.io/etcinit/shift:v0.0.0.4 generate -t git > CHANGELOG.md"
+	git add CHANGELOG.md
+	git commit -m "chore(CHANGELOG): Update CHANGELOG"
 
 dist:
 	rm -rf dist
@@ -16,6 +18,9 @@ upload: dist
 		&& svn add --force trunk/* \
 		&& svn ci -m 'Release. See github.com/sellerlabs/snagshout-wordpress.' \
 		--username snagshout
+
+checkout:
+	svn co https://plugins.svn.wordpress.org/snagshout remote
 
 clean:
 	rm -rf dist
--- contrib/tag.sh
+++ contrib/tag.sh
@@ -3,12 +3,13 @@
 echo "Please enter the new version tag in the X.X.X format: "
 read new_tag
 
-sed -e "s/Stable tag: [0-9]*\\.[0-9]*\\.[0-9]*/Stable tags: ${new_tag}/" \
-  src/readme.txt > src/readme.txt
-sed -e "s/@version [0-9]*\\.[0-9]*\\.[0-9]*/@version ${new_tag}/" \
-  src/snagshout.php > src/snagshout.php
-sed -e "s/Version: [0-9]*\\.[0-9]*\\.[0-9]*/Version: ${new_tag}/" \
-  src/snagshout.php > src/snagshout.php
+echo "${new_tag}"
+
+sed -i '' -e "s/Stable tag: [0-9]*\\.[0-9]*\\.[0-9]*/Stable tags: ${new_tag}/" \
+  src/readme.txt
+sed -i '' -e "s/@version [0-9]*\\.[0-9]*\\.[0-9]*/@version ${new_tag}/" \
+  -e "s/Version: [0-9]*\\.[0-9]*\\.[0-9]*/Version: ${new_tag}/" \
+  src/snagshout.php
 
 git add src/readme.txt src/snagshout.php
 git commit -m "chore(readme.txt): Bump version to ${new_tag}"
