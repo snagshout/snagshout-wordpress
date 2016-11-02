99d15a9ce2e6c40f714bf34c6841e49d3a768daa
Eduardo Trujillo <ed@sellerlabs.com>
2016-11-02 09:38:01 -0700

    fix(core): Use gmdate to ensure we are using UTC time

--- src/core/hashing.php
+++ src/core/hashing.php
@@ -19,7 +19,7 @@
 function snagshout_hash($secret_key, $body = '') {
   return hash_hmac(
     'sha512',
-    $body . (new DateTime())->format('Y-m-d H'),
+    $body . gmdate('Y-m-d H'),
     $secret_key
   );
 }
