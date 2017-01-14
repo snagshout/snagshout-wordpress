9388c1ad13599e8d9771ad50fdee10012f04837b
Eduardo Trujillo <ed@sellerlabs.com>
2017-01-13 17:28:40 -0800

    chore(readme.txt): Bump version to 0.4.7

--- src/snagshout.php
+++ src/snagshout.php
@@ -16,14 +16,14 @@
  *  limitations under the License.
  *
  *  @package Snagshout
- *  @version 0.4.6
+ *  @version 0.4.7
  *
  *  Plugin Name: Snagshout
  *  Plugin URI: https://www.snagshout.com
  *  Description: The Snagshout plugin allows you to easily embed syndicated
  *  deals into your Wordpress blog.
  *  Author: Seller Labs LLC
- *  Version: 0.4.6
+ *  Version: 0.4.7
  *  Author URI: https://www.sellerlabs.com
  */
 
